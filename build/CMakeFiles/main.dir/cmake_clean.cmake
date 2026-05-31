cde2d1a863180b6ca8f28baaa979647e98ff9350
LC235-com <2137713892@qq.com>
2026-05-31 20:03:23 +0800

    首次提交：上传项目初始代码

---
+++ build/CMakeFiles/main.dir/cmake_clean.cmake
@@ -0,0 +1,17 @@
+file(REMOVE_RECURSE
+  "C:/code/project1/bin/libmain.dll.a"
+  "C:/code/project1/bin/main.exe"
+  "C:/code/project1/bin/main.exe.manifest"
+  "C:/code/project1/bin/main.pdb"
+  "CMakeFiles/main.dir/src/main.c.obj"
+  "CMakeFiles/main.dir/src/main.c.obj.d"
+  "CMakeFiles/main.dir/src/print.c.obj"
+  "CMakeFiles/main.dir/src/print.c.obj.d"
+  "CMakeFiles/main.dir/src/print1.c.obj"
+  "CMakeFiles/main.dir/src/print1.c.obj.d"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/main.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
